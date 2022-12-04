1c13fc80f09a0bff47995103b89f74dbfe66f440
Pradeep Patel <119520997+pradeeppatel0300@users.noreply.github.com>
2022-12-04 17:41:32 +0530

    Create simple intrest.py

---
+++ simple intrest.py
@@ -0,0 +1,5 @@
+p=int(input("enter the value of principle"))
+r=int(input("enter the value of rate"))
+t=int(input("enter the value of time"))
+s=(p*r*t)/100
+print("the value of the simple intrest is",s)
